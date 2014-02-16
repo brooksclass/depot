e4af9b7e2365f78cc82d0e373820b0df17a93ba3
Brooks Childers <brooks@verasay.com>
2014-02-16 15:06:36 -0500

    page 146

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -32,7 +32,8 @@ class LineItemsController < ApplicationController
     @line_item = @cart.add_product(product.id)
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart }
+        format.html { redirect_to store_url }
+        format.js
         format.json { render action: 'show', status: :created, location: @line_item }
       else
         format.html { render action: 'new' }
@@ -60,7 +61,7 @@ class LineItemsController < ApplicationController
   def destroy
     @line_item.destroy
     respond_to do |format|
-      format.html { redirect_to @line_item.cart, notice: 'Line item was successfully removed.' }
+      format.html { redirect_to store_url, notice: 'Line item was successfully removed.' }
       format.json { head :no_content }
     end
   end
